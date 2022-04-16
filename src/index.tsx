545164064f048b63d42a010dfe6d135b89a090d2
blackkspydo <Upadhyaya13kishor@gmail.com>
2022-04-17 02:25:22 +0545

    added pwa files

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -19,3 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
