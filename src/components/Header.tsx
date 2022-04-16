9dcafba277ca2f44959c32998f6695ea9037fb3d
blackkspydo <Upadhyaya13kishor@gmail.com>
2022-04-16 19:50:47 +0545

    added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,16 @@ const Header = () => {
 	return (
 		<header className={styles.header}>
 			<Link to="/">
-				<h1 className={styles.title}>Img Compressor</h1>
+				<h1 className={styles.title}>Tinify.pro</h1>
 			</Link>
 			<nav>
 				<ul>
 					<li>
-						<Link to="/">Home</Link>
+						<Link to="/tools">Tools</Link>
 					</li>
+
 					<li>
 						<Link to="/about">About</Link>
-					</li>
-					<li>
-						<Link to="/contact">Contact</Link>
 					</li>
 				</ul>
 			</nav>
